de898afaac635b9f8c479c55f432c2ec28131e32
JonathanBerkeley <35268549+JonathanBerkeley@users.noreply.github.com>
2020-11-29 05:52:38 +0000

    Add files via upload

---
+++ Server/GameDevCAServer/GameDevCAServer/ThreadManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ * This is an external class
+ * Author: Tom Weiland
+ * https://www.youtube.com/watch?v=4uHTSknGJaY
+ */
+
+namespace GameDevCAServer
+{
+    class ThreadManager
+    {
+        private static readonly List<Action> executeOnMainThread = new List<Action>();
+        private static readonly List<Action> executeCopiedOnMainThread = new List<Action>();
+        private static bool actionToExecuteOnMainThread = false;
+
+
+
+        /// <summary>Sets an action to be executed on the main thread.</summary>
+        /// <param name="_action">The action to be executed on the main thread.</param>
+        public static void ExecuteOnMainThread(Action _action)
+        {
+            if (_action == null)
+            {
+                Console.Write("No action to execute on main thread!");
+                return;
+            }
+
+            lock (executeOnMainThread)
+            {
+                executeOnMainThread.Add(_action);
+                actionToExecuteOnMainThread = true;
+            }
+        }
+
+        /// <summary>Executes all code meant to run on the main thread. NOTE: Call this ONLY from the main thread.</summary>
+        public static void UpdateMain()
+        {
+            if (actionToExecuteOnMainThread)
+            {
+                executeCopiedOnMainThread.Clear();
+                lock (executeOnMainThread)
+                {
+                    executeCopiedOnMainThread.AddRange(executeOnMainThread);
+                    executeOnMainThread.Clear();
+                    actionToExecuteOnMainThread = false;
+                }
+
+                for (int i = 0; i < executeCopiedOnMainThread.Count; i++)
+                {
+                    executeCopiedOnMainThread[i]();
+                }
+            }
+        }
+    }
+}
