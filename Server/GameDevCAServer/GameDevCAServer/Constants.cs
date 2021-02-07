b765cc19b2949cb06dc5bb36fbf9de30bc27feec
JonathanBerkeley <35268549+JonathanBerkeley@users.noreply.github.com>
2021-02-07 21:22:24 +0000

    Add files via upload

---
+++ Server/GameDevCAServer/GameDevCAServer/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameDevCAServer
+{
+    class Constants
+    {
+        public const int TICKS_PER_SEC = 75;
+        public const int MS_PER_TICK = 1000 / TICKS_PER_SEC;
+        public const string SERVER_VERSION = "0.9.0";
+    }
+}
