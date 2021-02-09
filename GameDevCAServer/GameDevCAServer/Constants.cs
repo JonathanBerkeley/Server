3f86ca536f0e0b56f87c77cca382765b2a27e79c
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2021-02-09 13:41:20 +0000

    Diagnostics branch with advanced logging and lower tickrate added

--- GameDevCAServer/GameDevCAServer/Constants.cs
+++ GameDevCAServer/GameDevCAServer/Constants.cs
@@ -8,6 +8,6 @@ namespace GameDevCAServer
     {
         public const int TICKS_PER_SEC = 2;
         public const int MS_PER_TICK = 1000 / TICKS_PER_SEC;
-        public const string SERVER_VERSION = "0.9.2";
+        public const string SERVER_VERSION = "DIAG - 0.9.2";
     }
 }
