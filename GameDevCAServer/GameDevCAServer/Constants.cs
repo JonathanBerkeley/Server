0aabca512ec80342b656205aa4a670e097e1ea55
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2021-05-12 22:10:22 +0100

    Fix versioning

--- GameDevCAServer/GameDevCAServer/Constants.cs
+++ GameDevCAServer/GameDevCAServer/Constants.cs
@@ -8,7 +8,7 @@
         public const int MS_PER_TICK = 1000 / TICKS_PER_SEC;
         public const string SERVER_VERSION = "1.0.7";
         public static readonly string[] ACCEPTED_CLIENT_VERSIONS = { 
-            "1.2.8", "1.3.1", "1.3.2", "1.3.3", "1.3.4", "1.3.5", "1.3.6", "1.3.7", "1.3.8", "1.4.0"
+            "1.2.8", "1.3.1", "1.3.2", "1.3.3", "1.3.4", "1.3.5", "1.3.6", "1.3.7", "1.3.8", "1.4.0", "1.4.1"
         };
     }
 }
