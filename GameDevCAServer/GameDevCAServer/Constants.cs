b27ca1d9277b496f2513afc3fd611d34517fdec2
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2021-05-14 23:56:45 +0100

    Supports latest version, dropped support for older versions

--- GameDevCAServer/GameDevCAServer/Constants.cs
+++ GameDevCAServer/GameDevCAServer/Constants.cs
@@ -6,9 +6,9 @@
         public const int SERVER_PORT = 24745;
         public const int TICKS_PER_SEC = 75;
         public const int MS_PER_TICK = 1000 / TICKS_PER_SEC;
-        public const string SERVER_VERSION = "1.0.7";
-        public static readonly string[] ACCEPTED_CLIENT_VERSIONS = { 
-            "1.2.8", "1.3.1", "1.3.2", "1.3.3", "1.3.4", "1.3.5", "1.3.6", "1.3.7", "1.3.8", "1.4.0", "1.4.1"
+        public const string SERVER_VERSION = "1.0.8";
+        public static readonly string[] ACCEPTED_CLIENT_VERSIONS = {
+            "1.3.8", "1.4.0", "1.4.1", "1.4.5"
         };
     }
 }
